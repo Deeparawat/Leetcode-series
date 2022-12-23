96fac8f36c4fbe17a82ccabd80e27240c6670c3d
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2022-12-23 15:26:06 +0530

    Time: 19 ms (83.27%), Space: 19.9 MB (62.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,38 @@
 class Solution {
-public:     // brute force 
-       // tc- o(n)
-    //sc-o(n)
+public:
     int trap(vector<int>& height) {
-        int n= height.size();
-        vector<int>leftMax(n);
-        vector<int> rightMax(n);
+        int n =height.size();
         int Max=0;
-        int totalwater=0; 
-        
-        // storing all left max;
-        
+        int totalwater=0;
+        int maxIndex=0;
         for(int i=0;i<n;i++){
-            Max=max(Max,height[i]);
-            leftMax[i]=Max;
+            if(height[i]>Max){
+                Max=height[i];  // it is both right and left max;
+                maxIndex=i;
+            }
+        }
+            int leftMax=0;
+            // part 1
+            for(int i=0;i<maxIndex;i++){
+                leftMax= max(leftMax,height[i]);
+                int water=min(leftMax,Max)-height[i];
+                totalwater+=water;
+                
+            }
             
-        }
-        
-        Max=0;
-        //storing all right max
-        
-        for(int i=n-1; i >=0;i--){
-            Max=max(Max,height[i]);
-            rightMax[i]=Max;
-        }
-        
-        for(int i=0;i<n-1;i++){
-            int water=min(leftMax[i],rightMax[i])-height[i];
-            totalwater+=water;
-        }
+            //part2
+            leftMax=0;
+            for(int i=n-1;i>maxIndex;i--){
+                leftMax= max(leftMax,height[i]);
+                int water=min(leftMax,Max)-height[i];
+                totalwater+=water;
+                
+            }
         
         
-        return totalwater;
-    }
+            return totalwater;
+            }
+        
+        
+    
 };
