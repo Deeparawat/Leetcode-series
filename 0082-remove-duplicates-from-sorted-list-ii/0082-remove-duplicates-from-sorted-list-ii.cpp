153174f37bbe63f822baa2fc56be572b26758663
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2023-01-04 17:43:03 +0530

    Time: 23 ms (14.21%), Space: 12 MB (5.62%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -8,8 +8,12 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
+    
+    /*
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode* temp=new ListNode(0,head);//dummy node
         ListNode* prev=temp;
@@ -27,4 +31,53 @@ public:
         }
         return temp->next;
     }
+};
+
+
+
+ListNode* deleteDuplicates(ListNode* head) {
+    if(head == NULL || head->next == NULL) return head;
+    
+    ListNode* curr = head;
+    ListNode* prev = NULL;
+    while(curr != NULL && curr->next != NULL) {
+        if(curr->val == curr->next->val) {
+            while(curr->next != NULL && curr->val == curr->next->val) {
+                curr = curr->next;
+            }
+            if(prev != NULL) {
+                prev->next = curr->next;
+                curr = curr->next;    
+            }else {
+                head = curr->next;
+                curr = curr->next;   
+            }
+        }else {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+    return head;
+}
+
+*/
+
+ListNode* deleteDuplicates(ListNode* head) {
+        map<int, int> mp {};
+        while(head != NULL) {
+            mp[head->val] ++;
+            head = head->next;
+        }
+        ListNode *sol = new ListNode;
+        ListNode *p = sol;
+        for(auto it = mp.begin(); it != mp.end(); it ++)
+            if(it->second == 1) {
+                p->next = new ListNode(it->first);
+                p = p->next;
+            }
+        return sol->next;
+    }
+
+
+
 };
