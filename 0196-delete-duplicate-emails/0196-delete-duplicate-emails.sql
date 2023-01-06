bf861b76783fb3f416c69f6149d6eee7da16d7b0
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2023-01-06 23:40:29 +0530

    Time: 1174 ms (20.78%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,6 +1,10 @@
-# Please write a DELETE statement and DO NOT write a SELECT statement.
-# Write your MySQL query statement below
-
-DELETE FROM Person WHERE Id NOT IN 
-(SELECT * FROM(
-    SELECT MIN(Id) FROM Person GROUP BY Email) as id);
+/*
+ Please write a DELETE statement and DO NOT write a SELECT statement.
+ Write your PL/SQL query statement below
+ */
+DELETE FROM Person
+WHERE id IN (
+    SELECT P1.id
+    FROM Person P1 JOIN Person P2
+    ON(P1.id > P2.id AND P1.email = P2.email)
+);
