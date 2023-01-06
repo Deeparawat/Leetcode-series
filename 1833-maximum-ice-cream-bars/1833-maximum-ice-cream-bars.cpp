1d914d7a58448bdc377e3af832c9cc51a4f54655
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2023-01-06 20:14:59 +0530

    Time: 203 ms (79.27%), Space: 84.5 MB (5.18%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,26 +1,35 @@
+/*
+ tc-o(nlongn)
+ sc-o(1)
+
+*/
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int sum=0;
-        int count=0;
-        int rem=coins;
-        int n =costs.size();
-        for(int i=0;i<n;i++){
-            if(costs[i]<coins && costs[i]<=rem){
-                sum+=costs[i];
-                count=count+1;
-                rem=coins-sum;
-            }
-            else{
-                continue;
-            }
-             
-        
-        }  
-         return count;
-   
-            
-        
+        int cnt=0;
+        // sort(costs.begin(),costs.end());
+        // int cnt=0;
+        // for(int i=0;i<costs.size()&&coins>0;i++){
+        //     coins-=costs[i];
+        //     if(coins>=0)cnt++;
+        // }
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto it:costs){
+            pq.push(it);
+        }
+        while(!pq.empty()&&coins>0){
+            coins-=pq.top();
+            pq.pop();
+            if(coins>=0)cnt++;
+        }
+        // set<pair<int,int>>st;
+        // for(int i=0;i<costs.size();i++){
+        //     st.insert({costs[i],i});
+        // }
+        // for(auto it:st){
+        //     coins-=it.first;
+        //     if(coins>=0)cnt++;
+        // }
+        return cnt;
     }
 };
