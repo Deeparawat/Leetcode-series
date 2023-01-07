cff548134bdb05d567cfc27e2e898226b5dbdd78
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2023-01-07 22:38:28 +0530

    Time: 1289 ms (8.38%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,12 +1,12 @@
-/*
+
 SELECT DISTINCT a.Id
 FROM Weather a,Weather b
 WHERE a.Temperature > b.Temperature
 AND DATEDIFF(a.Recorddate,b.Recorddate) = 1
-*/
 
-
+/*
 
 SELECT w1.id
 FROM Weather AS w1 , Weather AS w2
 WHERE w1.Temperature > w2.Temperature AND DATEDIFF(w1.recordDate , w2.recordDate) = 1
+*/
