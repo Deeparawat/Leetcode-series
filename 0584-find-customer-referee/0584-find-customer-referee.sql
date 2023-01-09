1fd155d3a38cc0378332a9e4fd1e6e3838a4aec5
DEEPA RAWAT <39901983+Deeparawat@users.noreply.github.com>
2023-01-09 21:27:28 +0530

    Time: 666 ms (93.12%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
- select name from Customer where id not in (select id from customer where referee_id=2);
+/* Write your PL/SQL query statement below */
+select name from Customer where id not in (select id from customer where referee_id=2);
